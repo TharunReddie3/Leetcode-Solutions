d6133fa8a409423309d9186ebf42fd849e5598dc
TharunReddie3 <106477782+TharunReddie3@users.noreply.github.com>
2022-11-14 20:34:07 +0530

    Time: 1 ms (73.79%), Space: 42.2 MB (36.03%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,13 +1,6 @@
 import java.util.*;
 class Solution {
     public String defangIPaddr(String address) {
-        String s="[.]";
-        StringTokenizer st=new StringTokenizer(address+"",".");
-        String str="";
-        while(st.hasMoreTokens()){
-            str=str+s+st.nextToken();
-        }
-        str=str.substring(3,str.length());
-        return str;
+        return address.replace("." ,"[.]");
     }
 }
